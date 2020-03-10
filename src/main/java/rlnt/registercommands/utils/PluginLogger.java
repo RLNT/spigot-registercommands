8dcb77672f71d177486114d3b655acce35dcbb65
Chronophylos <nikolai@chronophylos.com>
2020-03-10 17:43:47 +0100

    reset colors after every record message

--- src/main/java/rlnt/registercommands/utils/PluginLogger.java
+++ src/main/java/rlnt/registercommands/utils/PluginLogger.java
@@ -3,7 +3,6 @@ package rlnt.registercommands.utils;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 
-import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
@@ -16,22 +15,7 @@ public class PluginLogger extends Logger {
 
     @Override
     public void log(LogRecord record) {
-        record.setMessage(ChatColor.translateAlternateColorCodes('&', record.getMessage()));
+        record.setMessage(ChatColor.translateAlternateColorCodes('&', record.getMessage())+ChatColor.RESET);
         super.log(record);
-    }
-
-    @Override
-    public void info(String msg) {
-        log(Level.INFO, msg + ChatColor.RESET);
-    }
-
-    @Override
-    public void warning(String msg) {
-        log(Level.WARNING, msg + ChatColor.RESET);
-    }
-
-    @Override
-    public void severe(String msg) {
-        log(Level.SEVERE, msg + ChatColor.RESET);
     }
 }
